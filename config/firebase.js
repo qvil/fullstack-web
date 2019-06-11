eed90333549315b577f004eeaa0dc0d261db0744
현태수 <qvil@op.gg>
2019-06-11 13:57:42 +0900

    now static deploy 위해서 폴더구조 변경

---
+++ config/firebase.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBUNGiia1uR7RCpFPXbBasaJkN-J8Umxmw",
+  authDomain: "fullstack-web.firebaseapp.com",
+  databaseURL: "https://fullstack-web.firebaseio.com",
+  projectId: "fullstack-web",
+  storageBucket: "",
+  messagingSenderId: "827465633963",
+  appId: "1:827465633963:web:662e2812c981b5b2"
+};
